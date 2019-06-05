ed7d3b26ee51040c1e952537c9d0b1ab6558f954
root <root@localhost.localdomain>
2019-06-05 14:12:44 +0300

    first commit

---
+++ src/components/edit-todo.component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class EditTodo extends Component{
+	render(){
+		return(
+			<div>
+				edit todo list
+			</div>
+			);
+	}
+}
+
+export default EditTodo;
